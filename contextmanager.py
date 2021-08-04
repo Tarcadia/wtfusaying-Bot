13960c7eeae1b8bead14c73d47cb5e3b36e76870
Tarcadia <tarcadia@qq.com>
2021-08-04 17:39:19 +0800

    小改

--- contextmanager.py
+++ contextmanager.py
@@ -4,5 +4,5 @@
 # 计算contextsensor的tid所指的topic下前count个关键词
 def topickeywords(cs : dict, tid : int, count : int = 3):
     _vals = sorted(cs['topics'][tid]['vec'].items(), key = lambda x : x[1], reverse = True);
-    _ans = _vals[range(min(count, len(_vals)))];
+    _ans = _vals[0 : min(count, len(_vals))];
     return _ans;
