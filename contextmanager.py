7a3953d7ae0de58d917fac185ee2d7f2e0fb6544
Tarcadia <tarcadia@qq.com>
2021-08-04 17:12:59 +0800

    笔误

--- contextmanager.py
+++ contextmanager.py
@@ -6,4 +6,4 @@ def topickeywords(cs : dict, tid : int, count : int = 3):
     _keys = [_k for _k in cs['topics'][tid]['vec']];
     _vals = [cs['topics'][tid]['vec'][_k] for _k in cs['topics'][tid]['vec']];
     _keys = [_keys[sorted(_vals).index(_)] for _ in _vals];
-    return _keys[0 : count];
+    return _keys[len(_keys) - count : len(_keys)];
