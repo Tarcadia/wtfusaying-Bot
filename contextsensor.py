7ee20d12455d6386c350acb86b88a31e905fb676
Tarcadia <tarcadia@qq.com>
2021-08-04 12:10:13 +0800

    添加log部分

--- contextsensor.py
+++ contextsensor.py
@@ -2,7 +2,19 @@
 import jieba;
 import jieba.analyse;
 import jieba.posseg;
+import logging
 import time;
+
+VERSION = 'v20210804';
+
+logger = logging.getLogger(__name__);
+logger.setLevel(logging.DEBUG);
+logger_ch = logging.StreamHandler();
+logger_ch.setLevel(logging.DEBUG);
+logger_formatter = logging.Formatter(format='[%(asctime)s][%(name)s][%(levelname)s] >> %(message)s', datefmt='%Y%m%d-%H%M%S');
+logger_ch.setFormatter(logger_formatter);
+logger.addHandler(logger_ch);
+logger.info('Sentence Generator Loaded');
 
 # contextsensor         : dict
 # {
