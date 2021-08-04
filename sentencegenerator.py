2a7ee006b44de5892a7ff4be22a597d7cf79f38e
Tarcadia <tarcadia@qq.com>
2021-08-04 12:44:36 +0800

    更新了sentencegenerator的注释

--- sentencegenerator.py
+++ sentencegenerator.py
@@ -21,12 +21,13 @@ logger.info('Sentence Generator Loaded');
 # patten                : dict
 # {
 #   'patten'            : str,          // patten名
-#   '<key1>'            : subsent       // patten内转义
-#   '<key2>'            : subsent       // patten内的转义
+#   '<key1>'            : subsent,      // patten内的转义
+#   '<key2>'            : subsent,      // patten内的转义
+#   ...
 #                                       // 在解析的时候会根据这里的key对patten展开的内容中的"#key"进行替换
 #                                       // 转义的作用域是本patten内的subsent及嵌套元素，不对patten内subsent转义后的内容进行转义
 #                                       // 逻辑上允许对展开的patten的patten名进行转义，但不建议
-# },
+# }
 
 # sentence              : list          // 一段语句的表达形式
 # [
@@ -36,6 +37,8 @@ logger.info('Sentence Generator Loaded');
 # subsent               : sentence, patten, str     // 语句的元素可以是一个patten，一个语句或者一个字符串
 
 
+# subsent = getpatten(pattenname);
+# 根据pattenname取patten中的一则subsent
 def getpatten(patten: str = ''):
     try:
         _fp = open(PATH_PATTEN + patten + '.json', encoding='utf-8');
@@ -47,6 +50,14 @@ def getpatten(patten: str = ''):
         logger.error('getpatten: unseccsessful get a patten');
         return '';
 
+# subsent = depatten(subsent, trans);
+# 将patten模板的subsent根据trans进行转义
+# trans                 : dict
+# {
+#   '#<key1>'           : subsent,      // 一条转义
+#   '#<key2>'           : subsent,
+#   ...
+# }
 def depatten(patten, trans: dict = dict()):
     if type(patten) == str:
         if patten in trans:
@@ -64,6 +75,8 @@ def depatten(patten, trans: dict = dict()):
         logger.error('depatten: unseccsessful depatten');
         return '';
 
+# str = translate(subsent);
+# 将subsent嵌套迭代翻译为字符串
 def translate(s = '', depth : int = 0):
     if depth > MAX_DEPTH:
         logger.error('translate: too deep translation');
