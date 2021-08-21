4a41f5cdca5f1b37224b84ae6b9ee5b0bc0c5b22
Tarcadia <tarcadia@qq.com>
2021-08-22 01:44:39 +0800

    实现了对键盘屏幕的封装，和mirai和tg的消息对等是做对bot的一个输入

---
+++ iomessagemanager.py
@@ -0,0 +1,153 @@
+
+import time;
+import json;
+import threading as thr;
+import logging;
+
+VERSION = 'v20210810';
+
+logger = logging.getLogger(__name__);
+logger.setLevel(logging.DEBUG);
+logger_ch = logging.StreamHandler();
+logger_ch.setLevel(logging.DEBUG);
+logger_formatter = logging.Formatter(fmt='[%(asctime)s][%(name)s][%(levelname)s] >> %(message)s', datefmt='%Y%m%d-%H%M%S');
+logger_ch.setFormatter(logger_formatter);
+logger.addHandler(logger_ch);
+logger.info('IO Message Manager Loaded');
+
+
+# iomessagemanager      : dict
+# {
+#   # 输出
+#   'print_lock'        :RLock,         // Print锁
+#   # 消息缓存队列
+#   'buffer_size'       : int = 1024,   // 缓存大小
+#   'buffer_lock'       : RLock,        // 缓存锁
+#   'buffer_recv'       : list          // 接收缓存
+# }
+
+
+# messagemanager = new()
+# 初始化一个messagemanager
+def new(
+    buffer_size         : int = 1024
+    ):
+    mm = {
+        'print_lock'    : thr.RLock(),
+        'buffer_size'   : buffer_size,
+        'buffer_lock'   : thr.RLock(),
+        'buffer_recv'   : list()
+    }
+    return mm;
+
+# messagemanager, bool = send(messagemanager, obj)
+# 进行一次向屏幕的发送
+def send(mm, data: dict = None):
+    _t = time.strftime('%Y%m%d-%H%M%S');
+    with mm['print_lock']:
+        for _k in dict:
+            print("[%16s] >> %6s : %s" % (_t, _k, data[_k]));
+    return mm, True;
+
+# messagemanager, obj = query(messagemanager)
+# 进行一次键盘接收
+def recv(mm):
+    _recv = input();
+    _recvs = _recv.split();
+
+    if len(_recvs) > 1:
+        _data = {
+            'call'          : _recvs[0],
+            'args'          : _recvs[1:]
+        }
+        
+    else:
+        _data = {
+            'msgs'          : _recvs
+        }
+    return mm, _data;
+
+# messagemanager, obj = query(messagemanager, obj)
+# 对一个messagemanager的连接进行一次查询，等待成功返回查询结构，或等待失败返回None
+def query(mm, data: dict = None):
+    mm = send(mm, data = data);
+    mm, _data = recv(mm);
+    return mm, _data;
+
+# messagemanager, succ = check(messagemanager)
+# 对一个messagemanager的连接进行一次查询，无内容返回False，有内容返回True
+def check(mm):
+    with mm['buffer_lock']:
+        if len(mm['buffer_recv']) > 0:
+            return mm, True;
+        else:
+            return mm, False;
+
+# messagemanager = do_recv(messagemanager)
+# 对一个messagemanager进行一轮接收
+def do_recv(mm):
+    mm, _data = recv(mm);
+    mm = send(mm, data = _data);
+    with mm['buffer_lock']:
+        if len(mm['buffer_recv']) < mm['buffer_size']:
+            mm['buffer_recv'].append(_data);
+        else:
+            mm['buffer_recv'].pop(0);
+            mm['buffer_recv'].append(_data);
+            logger.error('Full recv buffer');
+    return mm;
+
+
+class IOMessageManager:
+
+    def __init__(
+        self,
+        buffer_size     : int = 1024
+        ) -> None:
+
+        self._on_updt_polling = False;
+        self._th_updt_polling = None;
+
+        self._mm = new(
+            buffer_size = buffer_size
+        );
+        return None;
+    
+    def open(self):
+        return;
+
+    def close(self):
+        return;
+    
+    def send(self, data: dict = None):
+        self._mm, _ack = send(self._mm, data = data);
+        return _ack;
+    
+    def query(self, data: dict = None):
+        self._mm, _resp = query(self._mm, data = data);
+        return _resp;
+
+    def recv(self):
+        self._mm, _recv = recv(self._mm);
+        return _recv;
+
+    def check(self):
+        self._mm, _succ = check(self._mm);
+        return _succ;
+
+    def _updt_polling(self):
+        while self._on_updt_polling:
+            self._mm = do_recv(self._mm);
+        return;
+
+    def threadpolling(self):
+        self._on_updt_polling = True;
+        self._th_updt_polling = thr.Thread(
+            target = self._updt_polling,
+            name = 'IO Message Manager Update Polling'
+        );
+        return [self._th_updt_polling];
+    
+    def threadstop(self):
+        self._on_updt_polling = False;
+        self._th_updt_polling.join();
