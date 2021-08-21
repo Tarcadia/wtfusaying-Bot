030497cf144f9fca4d12b1de14f949ddcd92cb89
Tarcadia <tarcadia@qq.com>
2021-08-22 04:49:07 +0800

    fmt啊fmt

---
+++ mods/essential.py
@@ -0,0 +1,14 @@
+
+import logging;
+
+VERSION = 'v20210822';
+THREADS = [];
+
+logger = logging.getLogger(__name__);
+logger.setLevel(logging.DEBUG);
+logger_ch = logging.StreamHandler();
+logger_ch.setLevel(logging.DEBUG);
+logger_formatter = logging.Formatter(fmt='[%(asctime)s][%(name)s][%(levelname)s] >> %(message)s', datefmt='%Y%m%d-%H%M%S');
+logger_ch.setFormatter(logger_formatter);
+logger.addHandler(logger_ch);
+logger.info('Essential Loaded');
