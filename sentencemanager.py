cfc6a207f92cadde3644d3ed412f8b4e86a3ac72
Tarcadia <tarcadia@qq.com>
2021-08-23 06:14:32 +0800

    显示优化

--- sentencemanager.py
+++ sentencemanager.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__);
 logger.setLevel(logging.DEBUG);
 logger_ch = logging.StreamHandler();
 logger_ch.setLevel(logging.DEBUG);
-logger_formatter = logging.Formatter(format='[%(asctime)s][%(name)s][%(levelname)s] >> %(message)s', datefmt='%Y%m%d-%H%M%S');
+logger_formatter = logging.Formatter(format='[%(asctime)s][%(levelname)s][%(name)s] >> %(message)s', datefmt='%Y%m%d-%H%M%S');
 logger_ch.setFormatter(logger_formatter);
 logger.addHandler(logger_ch);
 logger.info('Sentence Manager Loaded');
