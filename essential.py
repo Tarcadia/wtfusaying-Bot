1a23de3fd2cff1da3e1c4784b73420380694d5aa
Tarcadia <tarcadia@qq.com>
2021-08-23 09:49:29 +0800

    把底层系统组件封装到essential里面，当然，essential和main还是水乳交融的状态，也不想去耦了，就当作是单纯的main的拓展文件吧

---
+++ essential.py
@@ -0,0 +1,328 @@
+
+import main;
+
+import os;
+import importlib;
+import logging;
+
+VERSION = 'v20210822';
+
+logger = logging.getLogger(__name__);
+logger.setLevel(logging.DEBUG);
+logger_ch = logging.StreamHandler();
+logger_ch.setLevel(logging.DEBUG);
+logger_formatter = logging.Formatter(fmt='[%(asctime)s][%(name)s][%(levelname)s] >> %(message)s', datefmt='%Y%m%d-%H%M%S');
+logger_ch.setFormatter(logger_formatter);
+logger.addHandler(logger_ch);
+logger.info('Essential Loaded');
+
+
+
+# 底层系统组件的定义：
+# _sys_cbs          : dict
+# {
+#   'fnc'           : function,     // 回调函数，详情请咨询BotControl中对回调的具体要求；
+#   'flt'           : flt,          // 过滤器以确定一条消息是否需要调用回调，详情请咨询BotControl中对回调的具体要求；
+#                                   // 明明可以在回调函数里面实现，为什么要拿出来呢？
+#                                   // 因为作者还有着最后一丝丝的对性能的微弱希冀，企图用数据flt的形式实现过滤；
+#   'key'           : str           // 回调函数的识别码，要求每个回调函数独一无二，建议用"_modname_funcname_xxx"的形式；
+# },
+# _sys_mods         : dict[modname] // 加载的模块组件的列表
+# {
+#   m1              : module,       // 各个模块组件
+#   m2              : module,
+#   m3              : module,
+#   ...
+# }
+# _sys_help_doc     : str           // 底层系统组件的help显示
+
+
+
+# 一个模块组件需要的定义实现：
+# 为一个单独的module的py，放在./mods/下以供调用，在调用时应当实现如下的接口
+# _mod_help_doc     : str,          // 形如ess._sys_help_doc，为模块组件的help内容
+# _mod_cbs          : list,         // 形如ess._sys_cbs，为模块组件要注册的callback列表
+# _botcontrol       : botcontrol    // 系统由此参数附回系统的botcontrol接口
+# start()                           // 模块的合法启动，返回自己的线程列表，虽然没用，但是这是要求
+# save()                            // 模块的保存操作
+# stop()                            // 模块的合法结束
+
+
+
+_sys_cbs = [];                      # 底层系统组件的 call back 列表
+_sys_mods = dict();                 # 引入mods列表
+_sys_help_doc = """
+    # 底层系统组件
+    help                                : 获取帮助
+    reload                              : 重载组件
+        -a                              : 重载全部组件
+        -m <m1>[ <m2> ...]              : 重载组件<m_i>
+    save                                : 调起存档
+    stop                                : 合法结束运行
+    """;                            # 底层系统组件的help doc
+
+
+
+# 底层系统组件实现
+
+def reloadall():
+    # 查找所有模块组件
+    _modname_list = [];
+    _files = os.listdir('./mods/');
+    logger.info('Detecting mods');
+    for _file in _files:
+        logger.info('Detected file %s' % _file);
+        _filename, _fileext = os.path.splitext(_file);
+        if _fileext == '.py':
+            logger.info('Accept file %s' % _file);
+            _modname = _filename;
+            _modname_list.append(_modname);
+    # 解注册当前所有模块组件接口，关闭当前所有模块组件
+    for _modname in _sys_mods:
+        _mod = _sys_mods[_modname];
+        logger.info('Import %s' % _modname);
+        try:
+            for _cb in _mod._mod_cbs:
+                main._bc.deregcallback(_cb['key']);
+        except KeyError:
+            logger.error('Failed dereg call back or Key Error');
+        except Exception as _err:
+            logger.error('Failed dereg call back %s with %s' % (_cb['key'], type(_err)));
+            logger.debug(_err);
+        try:
+            _mod.save();
+        except Exception as _err:
+            logger.error('Failed save mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+        try:
+            _mod.stop();
+        except Exception as _err:
+            logger.error('Failed stop mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+    # 重加载新查找的模块组件，开启模块组件，注册接口
+    _sys_mods.clear();
+    for _modname in _modname_list:
+        try:
+            _mod = importlib.import_module('mods.' + _modname);
+            _mod._botcontrol = main._bc;
+            _sys_mods[_modname] = _mod;
+        except Exception as _err:
+            logger.error('Failed import mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+        try:
+            _mod_thrs = _mod.start();
+            main.THREADS.extend(_mod_thrs);
+        except Exception as _err:
+            logger.error('Failed start mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+        try:
+            for _cb in _mod._mod_cbs:
+                main._bc.regcallback(_cb['fnc'], _cb['flt'], _cb['key']);
+        except KeyError:
+            logger.error('Failed reg call back or Key Error');
+        except Exception as _err:
+            logger.error('Failed reg call back %s with %s' % (_cb['key'], type(_err)));
+            logger.debug(_err);
+    return;
+
+def reloadmod(modlist: list = []):
+    for _modname in modlist:
+        # 查找已有模块组件，解注册模块组件接口，关闭模块组件
+        if _modname in _sys_mods:
+            _mod = _sys_mods[_modname];
+            try:
+                for _cb in _mod._mod_cbs:
+                    main._bc.deregcallback(_cb['key']);
+            except KeyError:
+                logger.error('Failed dereg call back or Key Error');
+            except Exception as _err:
+                logger.error('Failed dereg call back %s with %s' % (_cb['key'], type(_err)));
+                logger.debug(_err);
+            try:
+                _mod.save();
+            except Exception as _err:
+                logger.error('Failed save mod %s with %s' % (_modname, type(_err)));
+                logger.debug(_err);
+            try:
+                _mod.stop();
+            except Exception as _err:
+                logger.error('Failed stop mod %s with %s' % (_modname, type(_err)));
+                logger.debug(_err);
+            _sys_mods.pop(_modname);
+        # 重加载模块组件，开启模块组件，注册接口
+        if os.path.isfile('./mods/' + _modname + '.py'):
+            try:
+                _mod = importlib.import_module('mods.' + _modname);
+                _mod._botcontrol = main._bc;
+                _sys_mods[_modname] = _mod;
+            except Exception as _err:
+                logger.error('Failed import mod %s with %s' % (_modname, type(_err)));
+                logger.debug(_err);
+            try:
+                _mod_thrs = _mod.start();
+                main.THREADS.extend(_mod_thrs);
+            except Exception as _err:
+                logger.error('Failed start mod %s with %s' % (_modname, type(_err)));
+                logger.debug(_err);
+            try:
+                for _cb in _mod._mod_cbs:
+                    main._bc.regcallback(_cb['fnc'], _cb['flt'], _cb['key']);
+            except KeyError:
+                logger.error('Failed reg call back or Key Error');
+            except Exception as _err:
+                logger.error('Failed reg call back %s with %s' % (_cb['key'], type(_err)));
+                logger.debug(_err);
+        else:
+            logger.debug('Failed import mod %s for non-exists' % _modname);
+    return;
+
+def save():
+    for _modname in _sys_mods:
+        _mod = _sys_mods[_modname];
+        try:
+            _mod.save();
+        except Exception as _err:
+            logger.error('Failed save mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+    return;
+
+def stop():
+    # 关闭BotControl
+    try:
+        main._bc.threadstop();
+    except:
+        pass;
+    # 关闭各个MM
+    try:
+        main._iomm.threadstop();
+    except:
+        pass;
+    try:
+        main._mmm.threadstop();
+    except:
+        pass;
+    try:
+        main._tmm.threadstop();
+    except:
+        pass;
+    # 解注册当前所有模块组件接口，关闭当前所有模块组件
+    for _modname in _sys_mods:
+        _mod = _sys_mods[_modname];
+        try:
+            for _cb in _mod._mod_cbs:
+                main._bc.deregcallback(_cb['key']);
+        except KeyError:
+            logger.error('Failed dereg call back or Key Error');
+        except Exception as _err:
+            logger.error('Failed dereg call back %s with %s' % (_cb['key'], type(_err)));
+            logger.debug(_err);
+        try:
+            _mod.save();
+        except Exception as _err:
+            logger.error('Failed save mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+        try:
+            _mod.stop();
+        except Exception as _err:
+            logger.error('Failed stop mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+    print('已关闭');
+    return;
+
+
+
+# 底层系统组件接口
+
+_sys_cb_flt_echo = {'mmk' : {'mirai', 'telegram'}, 'msg' : {}};
+def _sys_cb_fnc_echo(mmk, msg):
+    logger.info(msg);
+
+_sys_cb_flt_help = {'mmk' : {'IO', 'Loopback'}, 'msg' : {'call' : 'help'}};
+def _sys_cb_fnc_help(mmk, msg):
+    main._bc.send('IO', _sys_help_doc);
+    for _modname in _sys_mods:
+        _mod = _sys_mods[_modname];
+        main._bc.send('IO', _mod._mod_help_doc);
+
+_sys_cb_flt_reload = {'mmk' : {'IO', 'Loopback'}, 'msg' : {'call' : 'reload'}};
+def _sys_cb_fnc_reload(mmk, msg):
+    if msg['args']:
+        if msg['args'][0] == '-a':
+            main._bc.send('IO', '开始重加载全部...');
+            reloadall();
+            main._bc.send('IO', '重加载完成');
+        elif msg['args'][0] == '-m':
+            try:
+                main._bc.send('IO', '开始重加载...');
+                reloadmod(msg['args'][1:]);
+                main._bc.send('IO', '重加载完成');
+            except Exception as _err:
+                main._bc.send('IO', '执行失败，help一下');
+                logger.error('Failed reload mod with %s' % type(_err));
+                logger.debug(_err);
+    else:
+        main._bc.send('IO', '参数不对，help一下');
+
+_sys_cb_flt_save = {'mmk' : {'IO', 'Loopback'}, 'msg' : {'call' : 'save'}};
+def _sys_cb_fnc_save(mmk, msg):
+    main._bc.send('IO', '开始保存...');
+    save();
+    main._bc.send('IO', '保存完成');
+
+_sys_cb_flt_stop = {'mmk' : {'IO', 'Loopback'}, 'msg' : {'call' : 'stop'}};
+def _sys_cb_fnc_stop(mmk, msg):
+    main._bc.send('IO', '正在关闭...');
+    global TO_STOP;
+    TO_STOP = True;
+    main._bc.send('IO', '已启动关闭线程');
+
+
+
+# 底层系统组件加载
+
+def load():
+
+    # 注册底层系统组件的callback
+    _sys_cbs.append({'fnc': _sys_cb_fnc_echo, 'flt': _sys_cb_flt_echo, 'key': '_sys_cb_echo'});
+    _sys_cbs.append({'fnc': _sys_cb_fnc_help, 'flt': _sys_cb_flt_help, 'key': '_sys_cb_help'});
+    _sys_cbs.append({'fnc': _sys_cb_fnc_reload, 'flt': _sys_cb_flt_reload, 'key': '_sys_cb_reload'});
+    _sys_cbs.append({'fnc': _sys_cb_fnc_save, 'flt': _sys_cb_flt_save, 'key': '_sys_cb_save'});
+    _sys_cbs.append({'fnc': _sys_cb_fnc_stop, 'flt': _sys_cb_flt_stop, 'key': '_sys_cb_stop'});
+
+    # 查找所有模块组件
+    _modname_list = [];
+    _files = os.listdir('./mods/');
+    logger.info('Detecting mods');
+    for _file in _files:
+        logger.info('Detected file %s' % _file);
+        _filename, _fileext = os.path.splitext(_file);
+        if _fileext == '.py':
+            logger.info('Accept file %s' % _file);
+            _modname = _filename;
+            _modname_list.append(_modname);
+    
+    # 加载查找的模块组件，开启模块组件，注册接口
+    for _modname in _modname_list:
+        try:
+            _mod = importlib.import_module('mods.' + _modname);
+            _mod._botcontrol = main._bc;
+            _sys_mods[_modname] = _mod;
+        except Exception as _err:
+            logger.error('Failed import mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+        try:
+            _mod_thrs = _mod.start();
+            main.THREADS.extend(_mod_thrs);
+        except Exception as _err:
+            logger.error('Failed start mod %s with %s' % (_modname, type(_err)));
+            logger.debug(_err);
+        try:
+            for _cb in _mod._mod_cbs:
+                main._bc.regcallback(_cb['fnc'], _cb['flt'], _cb['key']);
+        except KeyError:
+            logger.error('Failed reg call back or Key Error');
+        except Exception as _err:
+            logger.error('Failed reg call back %s with %s' % (_cb['key'], type(_err)));
+            logger.debug(_err);
+    
+    return;
--- main.py
+++ main.py
@@ -9,10 +9,9 @@ import loopbackmessagemanager as lbmm;
 import miraimessagemanager as mmm;
 import tgmessagemanager as tmm;
 
-import threading as thr;
+import essential as ess;
+
 import time;
-import os;
-import importlib;
 import logging;
 
 VERSION = 'v20210820';
@@ -38,8 +37,6 @@ logger.info('Main Loaded');
 # ================================================================ #
 #                      Tarcadia 20210823-0310                      #
 # ================================================================ #
-
-
 
 # 系统分为三部分：
 #
@@ -67,25 +64,16 @@ logger.info('Main Loaded');
 # 架构就写这里了，谁想改都可以改，只要充分的读完各个注释“定义”，然后在调dict的key的时候不会手残，那你应该开发起来还是很轻松的；
 # 其实定义都是写给编译器和IDE看的，我相信作为一名优秀的代码诗人，你一定能在没有自动补全和关键词修正的条件下，利用你完美的代码逻辑思维能力，完成伟大的编码工作；
 
+# ================================================================ #
+#                           系统架构和序言                          #
+# 关于系统的架构和实现方式的简述；                                   #
+# 对自己毫不负责的垃圾代码的阅读方式的简述；                          #
+# 对阅读到这里的你的期望。                                           #
+# ================================================================ #
+#                      Tarcadia 20210823-0939                      #
+# ================================================================ #
 
-
-# 底层系统组件的定义：
-# _sys_cbs          : dict
-# {
-#   'fnc'           : function,     // 回调函数，详情请咨询BotControl中对回调的具体要求；
-#   'flt'           : flt,          // 过滤器以确定一条消息是否需要调用回调，详情请咨询BotControl中对回调的具体要求；
-#                                   // 明明可以在回调函数里面实现，为什么要拿出来呢？
-#                                   // 因为作者还有着最后一丝丝的对性能的微弱希冀，企图用数据flt的形式实现过滤；
-#   'key'           : str           // 回调函数的识别码，要求每个回调函数独一无二，建议用"_modname_funcname_xxx"的形式；
-# },
-# _sys_mods         : dict[modname] // 加载的模块组件的列表
-# {
-#   m1              : module,       // 各个模块组件
-#   m2              : module,
-#   m3              : module,
-#   ...
-# }
-# _sys_help_doc     : str           // 底层系统组件的help显示
+# 好消息，我刚刚把底层系统组件的部分都塞进了essential.py里，只不过这部分组件还是和main水乳交融在一起，我感觉放过去可读性其实更差了，你只要知道essential是main中很多功能的具体实现就可以了
 
 
 
@@ -99,16 +87,6 @@ logger.info('Main Loaded');
 # 7. 由于以上的这些需求是杂糅的，所以就写在一起了；
 # 8. 没有将MessageManager的部分写进来，因为觉得main里写底层系统组件还是确实太肿了，这部分反正基本不会重载啥的，就不要搞了；
 
-
-
-# 一个模块组件需要的定义实现：
-# 为一个单独的module的py，放在./mods/下以供调用，在调用时应当实现如下的接口
-# _mod_help_doc     : str,          // 形如_sys_help_doc，为模块组件的help内容
-# _mod_cbs          : list,         // 形如_sys_cbs，为模块组件要注册的callback列表
-# _botcontrol       : botcontrol    // 系统由此参数附回系统的botcontrol接口
-# start()                           // 模块的合法启动，返回自己的线程列表，虽然没用，但是这是要求
-# save()                            // 模块的保存操作
-# stop()                            // 模块的合法结束
 
 
 # 初始化BotControl
@@ -135,261 +113,27 @@ _tmm = tmm.TgMessageManager(
 
 
 
-# 底层系统组件实现
 
-_sys_cbs = [];                      # 底层系统组件的 call back 列表
-_sys_mods = dict();                 # 引入mods列表
-_sys_help_doc = """
-# 底层系统组件
-help                                : 获取帮助
-reload                              : 重载组件
-    -a                              : 重载全部组件
-    -m <m1>[ <m2> ...]              : 重载组件<m_i>
-save                                : 调起存档
-stop                                : 合法结束运行
-""";                                # 底层系统组件的help doc
+# 主流程
 
-def reloadall():
-    # 查找所有模块组件
-    _modname_list = [];
-    _files = os.listdir('./mods/');
-    logger.info('Detecting mods');
-    for _file in _files:
-        logger.info('Detected file %s' % _file);
-        _filename, _fileext = os.path.splitext(_file);
-        if _fileext == '.py':
-            logger.info('Accept file %s' % _file);
-            _modname = _filename;
-            _modname_list.append(_modname);
-    # 解注册当前所有模块组件接口，关闭当前所有模块组件
-    for _modname in _sys_mods:
-        _mod = _sys_mods[_modname];
-        logger.info('Import %s' % _modname);
-        try:
-            for _cb in _mod._mod_cbs:
-                _bc.deregcallback(_cb['key']);
-        except KeyError:
-            logger.error('Failed dereg call back or Key Error');
-        except Exception as _err:
-            logger.error('Failed dereg call back %s with %s' % (_cb['key'], type(_err)));
-            logger.debug(_err);
-        try:
-            _mod.save();
-        except Exception as _err:
-            logger.error('Failed save mod %s with %s' % (_modname, type(_err)));
-            logger.debug(_err);
-        try:
-            _mod.stop();
-        except Exception as _err:
-            logger.error('Failed stop mod %s with %s' % (_modname, type(_err)));
-            logger.debug(_err);
-    # 重加载新查找的模块组件，开启模块组件，注册接口
-    _sys_mods.clear();
-    for _modname in _modname_list:
-        try:
-            _mod = importlib.import_module('mods.' + _modname);
-            _mod._botcontrol = _bc;
-            _sys_mods[_modname] = _mod;
-        except Exception as _err:
-            logger.error('Failed import mod %s with %s' % (_modname, type(_err)));
-            logger.debug(_err);
-        try:
-            _mod_thrs = _mod.start();
-            THREADS.extend(_mod_thrs);
-        except Exception as _err:
-            logger.error('Failed start mod %s with %s' % (_modname, type(_err)));
-            logger.debug(_err);
-        try:
-            for _cb in _mod._mod_cbs:
-                _bc.regcallback(_cb['fnc'], _cb['flt'], _cb['key']);
-        except KeyError:
-            logger.error('Failed reg call back or Key Error');
-        except Exception as _err:
-            logger.error('Failed reg call back %s with %s' % (_cb['key'], type(_err)));
-            logger.debug(_err);
-    return;
-
-def reloadmod(modlist: list = []):
-    for _modname in modlist:
-        # 查找已有模块组件，解注册模块组件接口，关闭模块组件
-        if _modname in _sys_mods:
-            _mod = _sys_mods[_modname];
-            try:
-                for _cb in _mod._mod_cbs:
-                    _bc.deregcallback(_cb['key']);
-            except KeyError:
-                logger.error('Failed dereg call back or Key Error');
-            except Exception as _err:
-                logger.error('Failed dereg call back %s with %s' % (_cb['key'], type(_err)));
-                logger.debug(_err);
-            try:
-                _mod.save();
-            except Exception as _err:
-                logger.error('Failed save mod %s with %s' % (_modname, type(_err)));
-                logger.debug(_err);
-            try:
-                _mod.stop();
-            except Exception as _err:
-                logger.error('Failed stop mod %s with %s' % (_modname, type(_err)));
-                logger.debug(_err);
-            _sys_mods.pop(_modname);
-        # 重加载模块组件，开启模块组件，注册接口
-        if os.path.isfile('./mods/' + _modname + '.py'):
-            try:
-                _mod = importlib.import_module('mods.' + _modname);
-                _mod._botcontrol = _bc;
-                _sys_mods[_modname] = _mod;
-            except Exception as _err:
-                logger.error('Failed import mod %s with %s' % (_modname, type(_err)));
-                logger.debug(_err);
-            try:
-                _mod_thrs = _mod.start();
-                THREADS.extend(_mod_thrs);
-            except Exception as _err:
-                logger.error('Failed start mod %s with %s' % (_modname, type(_err)));
-                logger.debug(_err);
-            try:
-                for _cb in _mod._mod_cbs:
-                    _bc.regcallback(_cb['fnc'], _cb['flt'], _cb['key']);
-            except KeyError:
-                logger.error('Failed reg call back or Key Error');
-            except Exception as _err:
-                logger.error('Failed reg call back %s with %s' % (_cb['key'], type(_err)));
-                logger.debug(_err);
-        else:
-            logger.debug('Failed import mod %s for non-exists' % _modname);
-    return;
-
-def save():
-    for _modname in _sys_mods:
-        _mod = _sys_mods[_modname];
-        try:
-            _mod.save();
-        except Exception as _err:
-            logger.error('Failed save mod %s with %s' % (_modname, type(_err)));
-            logger.debug(_err);
-    return;
-
-def stop():
-    # 关闭BotControl
-    try:
-        _bc.threadstop();
-    except:
-        pass;
-    # 关闭各个MM
-    try:
-        _iomm.threadstop();
-    except:
-        pass;
-    try:
-        _mmm.threadstop();
-    except:
-        pass;
-    try:
-        _tmm.threadstop();
-    except:
-        pass;
-    # 解注册当前所有模块组件接口，关闭当前所有模块组件
-    for _modname in _sys_mods:
-        _mod = _sys_mods[_modname];
-        try:
-            for _cb in _mod._mod_cbs:
-                _bc.deregcallback(_cb['key']);
-        except KeyError:
-            logger.error('Failed dereg call back or Key Error');
-        except Exception as _err:
-            logger.error('Failed dereg call back %s with %s' % (_cb['key'], type(_err)));
-            logger.debug(_err);
-        try:
-            _mod.save();
-        except Exception as _err:
-            logger.error('Failed save mod %s with %s' % (_modname, type(_err)));
-            logger.debug(_err);
-        try:
-            _mod.stop();
-        except Exception as _err:
-            logger.error('Failed stop mod %s with %s' % (_modname, type(_err)));
-            logger.debug(_err);
-    print('已关闭');
-    return;
-
-def stopwatch():
-    while not TO_STOP:
-        time.sleep(1);
-    stop();
-
-
-
-# 底层系统组件接口
-
-_sys_cb_flt_echo = {'mmk' : {'mirai', 'telegram'}, 'msg' : {}};
-def _sys_cb_fnc_echo(mmk, msg):
-    logger.info(msg);
-
-_sys_cb_flt_help = {'mmk' : {'IO'}, 'msg' : {'call' : 'help'}};
-def _sys_cb_fnc_help(mmk, msg):
-    _bc.send(mmk, _sys_help_doc);
-    for _modname in _sys_mods:
-        _mod = _sys_mods[_modname];
-        _bc.send(mmk, _mod._mod_help_doc);
-
-_sys_cb_flt_reload = {'mmk' : {'IO'}, 'msg' : {'call' : 'reload'}};
-def _sys_cb_fnc_reload(mmk, msg):
-    if msg['args']:
-        if msg['args'][0] == '-a':
-            _bc.send(mmk, '开始重加载全部...');
-            reloadall();
-            _bc.send(mmk, '重加载完成');
-        elif msg['args'][0] == '-m':
-            try:
-                _bc.send(mmk, '开始重加载...');
-                reloadmod(msg['args'][1:]);
-                _bc.send(mmk, '重加载完成');
-            except Exception as _err:
-                _bc.send(mmk, '执行失败，help一下');
-                logger.error('Failed reload mod with %s' % type(_err));
-                logger.debug(_err);
-    else:
-        _bc.send(mmk, '参数不对，help一下');
-
-_sys_cb_flt_save = {'mmk' : {'IO'}, 'msg' : {'call' : 'save'}};
-def _sys_cb_fnc_save(mmk, msg):
-    _bc.send(mmk, '开始保存...');
-    save();
-    _bc.send(mmk, '保存完成');
-
-_sys_cb_flt_stop = {'mmk' : {'IO'}, 'msg' : {'call' : 'stop'}};
-def _sys_cb_fnc_stop(mmk, msg):
-    _bc.send(mmk, '正在关闭...');
-    global TO_STOP;
-    TO_STOP = True;
-    _bc.send(mmk, '已启动关闭线程');
-
-_sys_cbs.append({'fnc': _sys_cb_fnc_echo, 'flt': _sys_cb_flt_echo, 'key': '_sys_cb_echo'});
-_sys_cbs.append({'fnc': _sys_cb_fnc_help, 'flt': _sys_cb_flt_help, 'key': '_sys_cb_help'});
-_sys_cbs.append({'fnc': _sys_cb_fnc_reload, 'flt': _sys_cb_flt_reload, 'key': '_sys_cb_reload'});
-_sys_cbs.append({'fnc': _sys_cb_fnc_save, 'flt': _sys_cb_flt_save, 'key': '_sys_cb_save'});
-_sys_cbs.append({'fnc': _sys_cb_fnc_stop, 'flt': _sys_cb_flt_stop, 'key': '_sys_cb_stop'});
-
-
-
-# main
 def main():
+    # 加载ess
+    ess.load();
     # 注册各个messagemanager类的接口到BotControl
     _bc.regmessagemanager(_mmm, 'mirai');
     _bc.regmessagemanager(_tmm, 'telegram');
     _bc.regmessagemanager(_iomm, 'IO');
     _bc.regmessagemanager(_lbmm, 'Loopback');
     # 注册各个callback接口到BotControl
-    for _cb in _sys_cbs:
+    for _cb in ess._sys_cbs:
         _bc.regcallback(_cb['fnc'], _cb['flt'], _cb['key']);
-    for _modname in _sys_mods:
-        _mod = _sys_mods[_modname];
+    for _modname in ess._sys_mods:
+        _mod = ess._sys_mods[_modname];
         for _cb in _mod._mod_cbs:
             _bc.regcallback(_cb['fnc'], _cb['flt'], _cb['key']);
     # 启动各个组件
-    for _modname in _sys_mods:
-        _mod = _sys_mods[_modname];
+    for _modname in ess._sys_mods:
+        _mod = ess._sys_mods[_modname];
         _mod_thrs = _mod.start();
         THREADS.extend(_mod_thrs);
     # 启动各个MM
@@ -401,6 +145,11 @@ def main():
     _bc_thrs = _bc.threadpolling();
     THREADS.extend(_bc_thrs);
     return;
+
+def stopwatch():
+    while not TO_STOP:
+        time.sleep(1);
+    ess.stop();
 
 if __name__ == '__main__':
     # 启动
