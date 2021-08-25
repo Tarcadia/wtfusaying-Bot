50f59d1b2945315ebd67ee79ec7c4f2919dda3a5
Tarcadia <tarcadia@qq.com>
2021-08-25 09:32:42 +0800

    再更新一下显示，把时间格式缩短后显示信息，不同类型的模块显示颜色加上区别

--- exs/sentencemanager.py
+++ exs/sentencemanager.py
@@ -13,7 +13,7 @@ logger = logging.getLogger(__name__);
 logger.setLevel(logging.DEBUG);
 logger_ch = logging.StreamHandler();
 logger_ch.setLevel(logging.DEBUG);
-logger_formatter = logging.Formatter(fmt='\033[1;34m[%(asctime)s][%(levelname)s]\033[0;32m[%(name)s]\033[0m >> %(message)s', datefmt='%Y%m%d-%H%M%S');
+logger_formatter = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;36m[%(name)s]\033[0m >> %(message)s', datefmt='%H:%M');
 logger_ch.setFormatter(logger_formatter);
 if not logger.hasHandlers():
     logger.addHandler(logger_ch);
---
+++ exs/tabot_totalk.py
@@ -0,0 +1,19 @@
+
+import CONSTS;
+
+import re;
+import logging;
+
+VERSION = 'v20210823';
+CONFIG = './config/autosave.json';
+
+logger = logging.getLogger(__name__);
+logger.setLevel(logging.DEBUG);
+logger_ch = logging.StreamHandler();
+logger_ch.setLevel(logging.DEBUG);
+logger_formatter = logging.Formatter(fmt='\033[0m%(asctime)s \033[1;34m[%(levelname)s]\033[0;36m[%(name)s]\033[0m >> %(message)s', datefmt='%H:%M');
+logger_ch.setFormatter(logger_formatter);
+if not logger.hasHandlers():
+    logger.addHandler(logger_ch);
+logger.info('Tabot To Talk - ex Loaded');
+
