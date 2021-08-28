fb930ea93a5bb4ebe922f79e8faf53d2f10c313a
Tarcadia <tarcadia@qq.com>
2021-08-29 06:11:34 +0800

    totalk的后台设计完成

--- exs/tabot_totalk.py
+++ exs/tabot_totalk.py
@@ -1,11 +1,18 @@
 
+import threading
 import CONSTS;
+
+import exs.wordlinksensor as xwls;
+import exs.contextsensor as xcs;
+import exs.topicsensor as xts;
+import exs.thermalmeter as xtm;
 
 import re;
 import logging;
 
 VERSION = 'v20210823';
-CONFIG = './config/tabot_totalk.json';
+CONFIG_PATH = './config/';
+WORDLINK_CFG = 'tabot_totalk_wls.json'
 
 logger = logging.getLogger(__name__);
 logger.setLevel(logging.DEBUG);
@@ -18,12 +25,85 @@ if not logger.hasHandlers():
 logger.info('Tabot To Talk - ex Loaded');
 
 # 数据记录：
-# chatkey = '<mmk>.<cid>';                      // 用于跨接口的标识语境
-# cid = '<g|p|c><xxxxxx>';                      // 用于同一个接口内跨类型的标识语境
-
+# chatkey = '<mmk>.<ctype><rcid>';              // 用于跨接口的标识语境
 # contexts = dict[chatkey]{contextsensor};      // 记录聊天上下文的contextsensor模块
+# thermals = dict[chatkey]{thermalmeter};       // 记录聊天热度和发言控制的thermalmeter模块
 # wordlinks = wordlinksensor;                   // 学习的结果，将会文件存储
-# 
+
+contexts = dict();
+thermals = dict();
+wordlink = xwls.WordLinkSensor(memorypath = CONFIG_PATH + WORDLINK_CFG);
+
+def onmsg(src, txt):
+    if src['cid'] in contexts and contexts[src['cid']]:
+        contexts[src['cid']].update(txt);
+    else:
+        contexts[src['cid']] = xcs.ContextSensor();
+        contexts[src['cid']].update(txt);
+    
+    if src['cid'] in thermals and thermals[src['cid']]:
+        thermals[src['cid']].onmsg(src['time']);
+    else:
+        if src['ctype'][0] == 'g':
+            thermals[src['cid']] = xtm.ThermalMeter(talkrate = 4);
+            thermals[src['cid']].onmsg(src['time']);
+        elif src['ctype'][0] == 'p':
+            thermals[src['cid']] = xtm.ThermalMeter(talkrate = 1);
+            thermals[src['cid']].onmsg(src['time']);
+        else:
+            thermals[src['cid']] = None;
+
+def oncall(src):
+    if src['cid'] in thermals and thermals[src['cid']]:
+        thermals[src['cid']].oncall(src['time']);
+    else:
+        if src['ctype'][0] == 'g':
+            thermals[src['cid']] = xtm.ThermalMeter(talkrate = 4);
+            thermals[src['cid']].oncall(src['time']);
+        elif src['ctype'][0] == 'p':
+            thermals[src['cid']] = xtm.ThermalMeter(talkrate = 1);
+            thermals[src['cid']].oncall(src['time']);
+        else:
+            thermals[src['cid']] = None;
+
+def ontalk(src):
+    if src['cid'] in thermals and thermals[src['cid']]:
+        thermals[src['cid']].ontalk(src['time']);
+    else:
+        if src['ctype'][0] == 'g':
+            thermals[src['cid']] = xtm.ThermalMeter(talkrate = 4);
+            thermals[src['cid']].ontalk(src['time']);
+        elif src['ctype'][0] == 'p':
+            thermals[src['cid']] = xtm.ThermalMeter(talkrate = 1);
+            thermals[src['cid']].ontalk(src['time']);
+        else:
+            thermals[src['cid']] = None;
+
+def cantalk(src, p: float = 1):
+    if src['cid'] in thermals and thermals[src['cid']]:
+        return thermals[src['cid']].cantalk(p = p);
+    else:
+        return False;
+    
+def trycantalk(src, p: float = 1):
+    if cantalk(src, p = p):
+        thermals[src['cid']].ontalk();
+        return True;
+    else:
+        return False;
+
+def strparams(src):
+    lines = [];
+    if src['cid'] in thermals and thermals[src['cid']]:
+        lines.append("当前对话前向度：%.3f\%" % thermals[src['cid']].value * 100);
+    else:
+        lines.append("当前对话前向度缺失");
+    if src['cid'] in contexts and contexts[src['cid']]:
+        cvec = contexts[src['cid']].get();
+        lines.append("当前上下文关键词：" + ','.join(['%s: %.1f' % (_k, cvec[_k]) for _k in cvec]));
+    else:
+        lines.append("当前上下文信息缺失");
+    return '\n'.join(lines);
 
 # 接口函数
 
