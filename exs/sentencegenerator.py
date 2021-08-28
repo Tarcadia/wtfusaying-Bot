e583cee377dc643a02846f1415cec12a8a6df5f6
Tarcadia <tarcadia@qq.com>
2021-08-29 06:20:53 +0800

    管理接口并入

--- exs/sentencegenerator.py
+++ exs/sentencegenerator.py
@@ -1,5 +1,7 @@
 
+import sys;
 import os;
+import fnmatch;
 import json;
 import random;
 import logging;
@@ -105,7 +107,71 @@ def translate(s = '', depth : int = 0):
             _trans = {('#' + _key) : s[_key] for _key in s if _key != 'patten'}
             _result = translate(depatten(_patten, _trans), depth + 1);
             return _result;
-            
-            
+
+
+
+
+
+# 管理功能接口
+
+def listpatten(filter: str = '*'):
+    _files = [];
+    for _fpath, _fdirs, _fnames in os.walk(PATH_PATTEN):
+        for _fname in _fnames:
+            if fnmatch.fnmatch(_fname, filter):
+                _file = _fpath + '/' + _fname;
+                _files.append(_file);
+    return json.dumps(_files, indent = 4);
+
+def showpatten(patten: str):
+    if os.path.isfile(PATH_PATTEN + patten + '.json'):
+        try:
+            _fp = open(PATH_PATTEN + patten + '.json', encoding='utf-8');
+            _pattens = json.load(_fp);
+            _fp.close();
+            return json.dumps(_pattens, indent = 4);
+        except:
+            return 'ERR: Unsuccsessful get a patten';
+    else:
+        return 'ERR: Unrecognized patten';
+
+def addpatten(patten: str, content: str):
+    if os.path.isfile(PATH_PATTEN + patten + '.json'):
+        try:
+            _fp = open(PATH_PATTEN + patten + '.json', encoding='utf-8');
+            _pattens = json.load(_fp);
+            _fp.close();
+        except:
+            return 'ERR: Unsuccsessful get a patten';
+
+        try:
+            _append = json.loads(content);
+            _pattens.append(_append);
+            _fp = open(PATH_PATTEN + patten + '.json', mode = 'w', encoding='utf-8');
+            json.dump(_pattens, _fp, indent = 4);
+            _fp.close();
+        except:
+            return 'ERR: Unsuccessful add to a patten';
+    else:
+        return 'ERR: Unrecognized patten';
+
+def newpatten(patten: str, content: str):
+    if not os.path.isfile(PATH_PATTEN + patten + '.json'):
+        try:
+            _patten = json.loads(content);
+            _fp = open(PATH_PATTEN + patten + '.json', mode = 'w', encoding='utf-8');
+            json.dump(_patten, _fp, indent = 4);
+            _fp.close();
+        except:
+            return 'ERR: Unsuccessful new a patten';
+    else:
+        return 'ERR: Existing patten';
+
+def runpatten(content: str):
+    try:
+        _content = json.loads(content);
+        return translate(_content);
+    except:
+        return 'ERR: Unsuccsessful decode: ' + json.dumps(sys.exc_info());
 
 
